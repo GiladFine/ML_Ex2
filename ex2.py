2d39e2f965b87603c55691ec76ad261132e02e86
Itai <itaishalik@gmail.com>
2019-05-20 20:18:34 +0300

    Base Class Added Perceptron Fixes

--- ex2.py
+++ ex2.py
@@ -34,114 +34,8 @@ class FileReader(object):
             self.data[i] = [self.data[i][0]] + [(self.data[i][j + 1] - min_values[j]) / (max_values[j] - min_values[j]) for j in range(len(self.data[i]) - 1)]
         print(self.data)
 
-class MultiClassPerceptron(object):
 
-    def __init__(self, classes, data, iterations, folds, eta):
-        self.classes = classes
-        self.data = [[data_item[0], data_item[1] + [BIAS]] for data_item in data]
-        self.num_of_features = len(self.data[0][1]) - 1
-        self.folds = folds
-        self.eta = eta
-        self.iterations = iterations
-
-        # Split feature data into train set, and test set
-        k, m = divmod(len(self.data), self.folds)
-        self.folded_data = list(self.data[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(self.folds))
-
-        self.train_set = [y for x in self.folded_data[1:] for y in x]
-        self.test_set = self.folded_data[0]
-
-        self.weights_arrays = {}
-
-
-    def predict(self, feature_input_data):
-        feature_input_array = np.array(feature_input_data)
-
-        max_activation = 0
-        prediction = self.classes[0]
-
-        for c in self.classes:
-            activation = np.dot(feature_input_array, self.weights_arrays[c])
-            if activation >= max_activation:
-                max_activation = activation
-                prediction = c
-
-        return prediction
-
-    def train(self):
-        self.weights_arrays = {c: np.array([0 for _ in range(self.num_of_features + 1)]) for c in self.classes}
-        for iter_num in range(self.iterations):
-            random.seed(315)
-            random.shuffle(self.train_set)
-            for category, feature_data in self.train_set:
-                feature_array = np.array(feature_data)
-
-                max_activation = 0
-                prediction = self.classes[0]
-
-                # Multi-Class Decision Rule:
-                for c in self.classes:
-                    activation = np.dot(feature_array, self.weights_arrays[c])
-                    if activation >= max_activation:
-                        max_activation = activation
-                        prediction = c
-
-                # Update Rule:
-                if not (category == prediction):
-                    self.weights_arrays[category] = np.array([(x + self.eta * y) for (x, y) in zip(self.weights_arrays[category], feature_array)])
-                    self.weights_arrays[prediction] = np.array([(x - self.eta * y) for (x, y) in zip(self.weights_arrays[prediction], feature_array)])
-
-    def cross_validate(self):
-        self.train()
-        accuracy_avg = self.accuracy()
-        for i in range(1, self.folds):
-            self.train_set += self.test_set
-            self.test_set = self.folded_data[i]
-            self.train_set = [x for x in self.train_set if x not in self.test_set]
-            self.train()
-            accuracy_avg += self.accuracy()
-        accuracy_avg /= self.folds
-        print("AVG Accuracy:", accuracy_avg)
-
-    def accuracy(self):
-        correct = 0
-        incorrect = 0
-        for category, feature_data in self.test_set:
-            prediction = self.predict(feature_data)
-            if category == prediction:
-                correct += 1
-            else:
-                incorrect += 1
-
-        accuracy = (correct * 1.0) / ((correct + incorrect) * 1.0)
-        print ("PERCEPTRON ACCURACY:")
-        print ("Model Accuracy:", accuracy)
-        return accuracy
-
-class SVM:
-    def __init__(self, classes, data, iterations, folds, eta, Lambda):
-
-        # Split feature data into train set, and test set
-        self.classes = classes
-        self.data = data
-        self.iterations = iterations
-        self.folds = folds
-        self.eta = eta
-        self.Lambda = Lambda
-
-        k, m = divmod(len(self.data), self.folds)
-        self.folded_data = list(self.data[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(self.folds))
-
-        self.train_set = [y for x in self.folded_data[1:] for y in x]
-        self.test_set = self.folded_data[0]
-
-        self.train_x = [line[1] for line in self.train_set]
-        self.train_y = [line[0] for line in self.train_set]
-
-        self.test_x = [line[1] for line in self.test_set]
-        self.test_y = [line[0] for line in self.test_set]
-
-        self.w = np.zeros((len(self.classes), len(self.train_x[0])))
+class MLAlgorithm(object):
 
     def cross_validate(self):
         self.train()
@@ -176,9 +70,84 @@ class SVM:
                 incorrect += 1
 
         accuracy = (correct * 1.0) / ((correct + incorrect) * 1.0)
-        print ("SVM ACCURACY:")
-        print ("Model Accuracy:", accuracy)
+        print("Model Accuracy:", accuracy)
         return accuracy
+
+class MultiClassPerceptron(MLAlgorithm):
+
+    def __init__(self, classes, data, iterations, folds, eta):
+        self.classes = classes
+        self.data = [[data_item[0], data_item[1] + [BIAS]] for data_item in data]
+        self.num_of_features = len(self.data[0][1]) - 1
+        self.folds = folds
+        self.eta = eta
+        self.iterations = iterations
+
+        # Split feature data into train set, and test set
+        k, m = divmod(len(self.data), self.folds)
+        self.folded_data = list(self.data[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(self.folds))
+
+        self.train_set = [y for x in self.folded_data[1:] for y in x]
+        self.test_set = self.folded_data[0]
+
+        self.train_x = [line[1] for line in self.train_set]
+        self.train_y = [line[0] for line in self.train_set]
+
+        self.test_x = [line[1] for line in self.test_set]
+        self.test_y = [line[0] for line in self.test_set]
+
+        self.w = np.zeros((len(self.classes), len(self.train_x[0])))
+
+    def train(self):
+        self.w = np.zeros((len(self.classes), len(self.train_x[0])))
+        for iter_num in range(self.iterations):
+            random.seed(315)
+            random.shuffle(self.train_set)
+            for category, feature_data in self.train_set:
+                feature_array = np.array(feature_data)
+
+                max_activation = 0
+                prediction = self.classes[0]
+
+                # Multi-Class Decision Rule:
+                for i in range(len(self.classes)):
+                    activation = np.dot(feature_array, self.w[i])
+                    if activation >= max_activation:
+                        max_activation = activation
+                        prediction = self.classes[i]
+
+                # Update Rule:
+                if not (category == prediction):
+                    class_index = self.classes.index(category)
+                    pred_index = self.classes.index(prediction)
+                    self.w[class_index] = np.array([(x + self.eta * y) for (x, y) in zip(self.w[class_index], feature_array)])
+                    self.w[pred_index] = np.array([(x - self.eta * y) for (x, y) in zip(self.w[pred_index], feature_array)])
+
+class SVM(MLAlgorithm):
+    def __init__(self, classes, data, iterations, folds, eta, Lambda):
+
+        # Split feature data into train set, and test set
+        self.classes = classes
+        self.data = data
+        self.iterations = iterations
+        self.folds = folds
+        self.eta = eta
+        self.Lambda = Lambda
+
+        k, m = divmod(len(self.data), self.folds)
+        self.folded_data = list(self.data[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(self.folds))
+
+        self.train_set = [y for x in self.folded_data[1:] for y in x]
+        self.test_set = self.folded_data[0]
+
+        self.train_x = [line[1] for line in self.train_set]
+        self.train_y = [line[0] for line in self.train_set]
+
+        self.test_x = [line[1] for line in self.test_set]
+        self.test_y = [line[0] for line in self.test_set]
+
+        self.w = np.zeros((len(self.classes), len(self.train_x[0])))
+
 
     def train(self):
         # Support Vector Machine algorithm
@@ -209,7 +178,7 @@ class SVM:
         return self.w
 
 
-class PA:
+class PA(MLAlgorithm):
     def __init__(self, classes, data, iterations, folds):
         # Split feature data into train set, and test set
         self.classes = classes
@@ -229,43 +198,6 @@ class PA:
         self.test_y = [line[0] for line in self.test_set]
 
         self.w = np.zeros((len(self.classes), len(self.train_x[0])))
-
-    def cross_validate(self):
-        self.train()
-        accuracy_avg = self.accuracy()
-        for i in range(1, self.folds):
-            self.train_set += self.test_set
-            self.test_set = self.folded_data[i]
-            self.train_set = [x for x in self.train_set if x not in self.test_set]
-            self.train()
-            accuracy_avg += self.accuracy()
-        accuracy_avg /= self.folds
-        print("AVG Accuracy:", accuracy_avg)
-
-    def predict(self, test_x):
-        N = len(test_x)
-        y_hats = np.zeros(N, dtype=int)
-        for i in range(N):
-            x = test_x[i]
-            values = np.dot(self.w, x)
-            y_hat = np.argmax(values)
-            y_hats[i] = y_hat
-        return y_hats
-
-    def accuracy(self):
-        correct = 0
-        incorrect = 0
-        results = self.predict(self.test_x)
-        for i in range(len(results)):
-            if results[i] == self.test_y[i]:
-                correct += 1
-            else:
-                incorrect += 1
-
-        accuracy = (correct * 1.0) / ((correct + incorrect) * 1.0)
-        print ("PA ACCURACY:")
-        print ("Model Accuracy:", accuracy)
-        return accuracy
 
     def train(self):
         #PA algorithm
@@ -308,13 +240,16 @@ def main():
 
     data = list(zip(reader.classes, reader.data))
 
+    print("Perceptron ACCURACY:")
     perceptron = MultiClassPerceptron(CLASSES, data, iterations=25, folds=4, eta=0.2)
     perceptron.cross_validate()
 
     svm = SVM(CLASSES, data, iterations=40, folds=4, eta=0.01, Lambda=0.5)
+    print("SVM ACCURACY:")
     svm.cross_validate()
 
     pa = PA(CLASSES, data, iterations=100, folds=4)
+    print("PA ACCURACY:")
     pa.cross_validate()
 
 if __name__ == "__main__":
